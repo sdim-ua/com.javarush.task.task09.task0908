dcd509d20d8cdec9912bff4c32a31afca39b3b73
sdim-ua <ingersol@yandex.ua>
2019-03-28 22:13:49 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public static void main(String[] args) throws Exception {
+       
+       try{
+        //напишите тут ваш код
+
+        String s = null;
+        String m = s.toLowerCase();
+
+        //напишите тут ваш код
+       }
+       catch(NullPointerException e){
+           
+           System.out.println(e.getClass());
+       }
+        
+        
+    }
+}
